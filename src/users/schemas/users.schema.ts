51db8b76b87109e737a850f3366646905a71d5bc
hengage <tunecarterkillz@gmail.com>
2024-05-17 02:03:58 +0100

    Feature: user schema

---
+++ src/users/schemas/users.schema.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+import * as bcrypt from 'bcrypt';
+import { HydratedDocument } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true })
+  firstName: string;
+
+  @Prop({ required: true })
+  lastName: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+export type UserDocument = HydratedDocument<User>;
+
+UserSchema.pre('save', async function (next) {
+  const user = this as UserDocument;
+
+  if (!user.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = bcrypt.hashSync(user.password, salt);
+
+  return next();
+});
