98d9d5162db788c24008e1da356f9208c4c2a93e
hengage <tunecarterkillz@gmail.com>
2024-05-17 18:16:32 +0100

    Feature: user validation

--- src/users/dto/user,dto.ts
+++ src/users/dto/user,dto.ts
@@ -1,6 +1,21 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
 export class CreateUserDTO {
+  @IsNotEmpty({ message: 'First name is required' })
+  @IsString({ message: 'First name must be a string' })
   firstName: string;
+
+  @IsNotEmpty({ message: 'Last name is required' })
+  @IsString({ message: 'Last name must be a string' })
   lastName: string;
+
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsString({ message: 'Email must be a string' })
+  @MaxLength(50)
+  @IsEmail({}, { message: 'Email must be a valid email format' })
   email: string;
+
+  @IsNotEmpty({ message: 'Password is required' })
+  @IsString({ message: 'Password must be a string' })
   password: string;
 }
