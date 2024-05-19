1082e95858529c53c97d8af3b4196874da85c883
hengage <tunecarterkillz@gmail.com>
2024-05-19 23:23:24 +0100

    Validation: forbid non-whitelisted properties

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { corsOptions } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strip properties that do not have any decorators
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
+    }),
+  );
   app.enableCors(corsOptions);
   await app.listen(3000);
 }
